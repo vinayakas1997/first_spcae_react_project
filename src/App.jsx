6b445288611e6452ffed5536642c333c948aeaea
vinayakas1997 <sajjanshetty-vinayaka@somic.inc>
2025-09-19 15:57:33 +0900

    added the background the points to keep note is z-[-1]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import bgVideo from './assets/earth-bg.mp4';
 const App = () => {
-  return <div></div>
+  return <div>
+    <div className="h-[700px] relative">
+      <video autoPlay loop muted 
+      className='fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]'>
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+
+    </div>
+  </div>
   
 }
 
