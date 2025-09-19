9146daded9e14e19b497ed2e33a97666f55aec31
vinayakas1997 <sajjanshetty-vinayaka@somic.inc>
2025-09-19 16:51:34 +0900

    completed navbar items, focus points on the ul and li , span

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bgVideo from './assets/earth-bg.mp4';
+import Navbar from './components/Navbar/Navbar';
 const App = () => {
   return <div>
     <div className="h-[700px] relative">
@@ -7,7 +8,8 @@ const App = () => {
       className='fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]'>
         <source src={bgVideo} type="video/mp4" />
       </video>
-
+      <Navbar/>
+      <Hero />
     </div>
   </div>
   
