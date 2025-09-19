2914e8bc6bc7af8f5e9f7e9a8fb7110f2e4e497e
vinayakas1997 <sajjanshetty-vinayaka@somic.inc>
2025-09-19 15:23:42 +0900

    add the conatiner and proeprties in the tailwind config - 1rem=16px

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center:true,
+        padding:{
+          DEFAULT: "1rem",
+          sm: "2rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
